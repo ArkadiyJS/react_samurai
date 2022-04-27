4cc403e2fe077f9fd8bb594374a1aa391afccde3
Arkadiy Vologodskiy <Vologodskiy20@gmail.com>
2022-04-27 14:40:58 +0500

    перекинул пропсы изи

--- src/App.js
+++ src/App.js
@@ -9,13 +9,9 @@ import { Route, Routes } from 'react-router-dom';
 
 
 function App(props) {
-  let posts = [
-    {id:1, message: 'hi gayz', likesCount:12 },
-    {id:2, message: 'hi gayz mayz', likesCount:15 },
-    {id:2, message: 'hi dudu', likesCount:15 },
-    {id:2, message: 'hi gara dada', likesCount:15 },
-    
-  ]
+  
+  
+
  
   
   return (
@@ -26,8 +22,8 @@ function App(props) {
 
       <div className='app-wraper-content' >
         <Routes>
-          <Route path='/dialogs' element={<Dialogs />}></Route>
-          <Route path='/profile' element={<ProfileCore posts={posts} />}></Route>
+          <Route path='/dialogs' element={<Dialogs dialogData={props.dialogData} messagesData={props.messagesData}/>}></Route>
+          <Route path='/profile' element={<ProfileCore posts={props.posts} />}></Route>
         </Routes>
       </div>
       <Footer />
--- src/components/dialogs/dialogs.jsx
+++ src/components/dialogs/dialogs.jsx
@@ -9,29 +9,15 @@ import Message from "./messages/messages";
 
 
 function Dialogs (props) {
-
-    let dialogData = [
-        { id: 1, name:'ark'},
-        { id: 2, name:'sveta'},
-        { id: 3, name:'matvey'},
-        { id: 4, name:'sofa'},
-        { id: 5, name:'kuzya'},
-
-    ]
-    let dialogsElements = dialogData
+    
+    
+    let dialogsElements = props.dialogData
         .map( (dialog) => <DialogItem name={dialog.name} id={dialog.id}/> ); 
     
     
-    let messagesData = [
-        { id: 1, name:'ark message'},
-        { id: 2, name:'sveta message'},
-        { id: 3, name:'matvey message'},
-        { id: 4, name:'sofa message'},
-        { id: 5, name:'kuzya message'},
+   
 
-    ]
-
-    let messageElements = messagesData
+    let messageElements = props.messagesData
     .map ((message)=> <Message message={message.message}/>);
 
 
