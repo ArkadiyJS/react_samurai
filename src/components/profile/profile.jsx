aedf5d689fbb86efc78aaadebf46ad45360d6440
Arkadiy Vologodskiy <Vologodskiy20@gmail.com>
2022-04-27 14:17:14 +0500

    данные постов в апп

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -4,13 +4,7 @@ import MyPosts from './myPosts/myPosts';
 import ProfileInfo from './profileinfo/profileinfo';
 
 function ProfileCore(props) {
-    let posts = [
-        {id:1, message: 'hi gayz', likesCount:12 },
-        {id:2, message: 'hi gayz mayz', likesCount:15 },
-        {id:2, message: 'hi dudu', likesCount:15 },
-        {id:2, message: 'hi gara dada', likesCount:15 },
-        
-    ]
+    
     return (
         <div className={s.profile}>
             
