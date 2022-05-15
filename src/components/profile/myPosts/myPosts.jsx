558daa86ccd9caa237a5da78a5a7338f9e2070c4
Arkadiy Vologodskiy <Vologodskiy20@gmail.com>
2022-05-15 20:07:22 +0500

    rofle mosue over/out

--- src/components/profile/myPosts/myPosts.jsx
+++ src/components/profile/myPosts/myPosts.jsx
@@ -7,24 +7,23 @@ function MyPosts(props) {
 
     let postElements = props.posts.map((p)=><Post message={p.message} likesCount={p.likesCount} />)
 
-    let  addPost = () => { 
-        let text = newPostElement.current.value;
-        
+    
+    let addPost =()=>{
+        let text = dataNewPost.current.value;
         alert(text)
+    }
+    let dataNewPost=React.createRef();
 
-    };
-
-
-    let newPostElement = React.createRef();
+    
 
 
     return (
         <div className={s.myPosts}>
             <div>
-                <textarea ref={newPostElement}>здесь будет текст</textarea>
+                <textarea ref={dataNewPost}>здесь будет текст</textarea>
             </div>
             <div>
-                <button onClick={addPost}>add post</button>
+                <button onMouseOut={addPost}>add post</button>
             </div>
 
             <div className={s.posts}> my posts</div>
