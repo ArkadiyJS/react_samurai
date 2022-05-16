f55446993457feb96330ff611549bc4b6d3ad13c
Arkadiy Vologodskiy <Vologodskiy20@gmail.com>
2022-05-16 17:14:38 +0500

    render

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from './components/footer/footers';
 import Dialogs from './components/dialogs/dialogs';
 import ProfileCore from './components/profile/profile';
 import { Route, Routes } from 'react-router-dom';
+import { addPost } from './redux/state';
 
 
 function App(props) {
@@ -23,7 +24,7 @@ function App(props) {
       <div className='app-wraper-content' >
         <Routes>
           <Route path='/dialogs' element={<Dialogs dialogData={props.state.dialogsPage.dialogData} messagesData={props.state.dialogsPage.messagesData}/>}></Route>
-          <Route path='/profile' element={<ProfileCore posts={props.state.profilePage.posts} />}></Route>
+          <Route path='/profile' element={<ProfileCore posts={props.state.profilePage.posts} addPost={props.addPost} />}></Route>
         </Routes>
       </div>
       <Footer />
