cde24362fe8771d65154c8e7471a3c4143a86f8b
Arkadiy Vologodskiy <Vologodskiy20@gmail.com>
2022-05-19 22:46:56 +0500

    music скелет

---
+++ src/components/musiс/music.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import s from './music.module.css';
+
+
+
+
+
+function MusicContent () {
+    return (
+        <div className={s.musicContent}>
+
+            text
+            
+        </div>
+    )
+};
+export default MusicContent;
