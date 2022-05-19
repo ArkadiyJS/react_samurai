09018888c5ccf2c3b71c415168f786f3803d6101
Arkadiy Vologodskiy <Vologodskiy20@gmail.com>
2022-05-19 22:47:49 +0500

    настройки скелет

---
+++ src/components/setting/setting.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import s from './setting.module.css';
+
+function Setting () {
+    return (
+        <div className={s.setting}>
+            setting
+        </div>
+    )
+
+};
+export default Setting;
