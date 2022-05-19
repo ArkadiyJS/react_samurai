88ed56bdea5ea3e5ca26b14063c92a88ab68045c
Arkadiy Vologodskiy <Vologodskiy20@gmail.com>
2022-05-19 22:47:25 +0500

    новости скелет

---
+++ src/components/news/news.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import s from './news.module.css';
+
+
+function News (){
+    return(
+        <div className={s.news}>
+            Новости
+        </div>
+    )
+};
+
+export default News;
