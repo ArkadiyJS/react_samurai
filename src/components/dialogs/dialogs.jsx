9d56b85db24e5848924563ec332eb61e64602ea0
Arkadiy Vologodskiy <Vologodskiy20@gmail.com>
2022-05-30 20:34:30 +0500

    good job dialogs

--- src/components/dialogs/dialogs.jsx
+++ src/components/dialogs/dialogs.jsx
@@ -3,32 +3,42 @@ import React from "react";
 import s from "./dialogs.module.css";
 import DialogItem from "./dialogItem/dialogItem";
 import Message from "./messages/messages";
-import { sendMessageCreator, updateNewMessageBodyCreator } from "../../redux/dialogsReducer";
+
 
 
 
 
 
 function Dialogs (props) {
-    
-    
-    
+    let state = props.dialogsPage;
+    let dialogsElement= state.dialogs.map(d=><DialogItem name={d.name} id={d.id}/>)
+    let messagesElements = state.messages.map(m=><Message message={m.message}/>)
+    let onNewMessagebody= state.newMessagebody;
 
+
+    let onSendMessageClick=()=>{
+        props.sendMessage();
+    }
+    
+    let onNewMessageChange = (e) =>{
+        let body = e.target.value;
+        props.updateNewMessageBody(body);
+    }
 
     return (
 
         <div className={s.dialogs}>
             <div className={s.dialogs__item}>
-           <DialogItem/>
-            
+           
+            {dialogsElement}
             
             </div>
             
             <div className={s.messages__item}>
-
-               <Message/>
-               <textarea  ></textarea>
-               <button >add message</button>
+                {messagesElements}
+               
+               <textarea value={onNewMessagebody}  onChange={onNewMessageChange}></textarea>
+               <button onClick={onSendMessageClick} >add message</button>
 
             </div>
 
