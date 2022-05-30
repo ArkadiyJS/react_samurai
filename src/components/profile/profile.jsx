fd5c3db6a756e4665b68156530f8e1e799ae7347
Arkadiy Vologodskiy <Vologodskiy20@gmail.com>
2022-05-30 20:34:52 +0500

    compain

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import s from './profile.module.css';
-import MyPosts from './myPosts/myPosts';
+import MyPostsContainer from './myPosts/myPostsContainer';
 import ProfileInfo from './profileinfo/profileinfo';
 
 function ProfileCore(props) {
+    
+    
     
     return (
         <div className={s.profile}>
             
             <div>
                 <ProfileInfo/>
-                <MyPosts     />
+                <MyPostsContainer  store={props.store} state={props.state}  />
             </div>
 
 
