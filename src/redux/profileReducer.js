b1261f50293cb94b22da0efe726fce59278053e0
Arkadiy Vologodskiy <Vologodskiy20@gmail.com>
2022-06-08 07:41:09 +0500

    1

--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -6,8 +6,8 @@ let initialState ={
     posts: [
       { id: 1, message: 'hi gayz', likesCount: 12 },
       { id: 2, message: 'hi gayz mayz', likesCount: 15 },
-      { id: 2, message: 'hi dudu', likesCount: 15 },
-      { id: 2, message: 'hi gara dada', likesCount: 15 },
+      { id: 3, message: 'hi dudu', likesCount: 15 },
+      { id: 4, message: 'hi gara dada', likesCount: 15 },
     ],
     newPostText: 'samurai'
   ,}
