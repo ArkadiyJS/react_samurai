361a9a20b3988c2e2d1361092feed9c0a65f2292
Arkadiy Vologodskiy <Vologodskiy20@gmail.com>
2022-06-08 21:16:54 +0500

    solo probnik users

---
+++ src/components/users/usersbar/usersBar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import s from './usersBar.module.css'
+
+
+function UsersBar (props){
+    let followTry =()=>{
+        props.followUp()
+    }
+    -
+    return(
+        <div className={s.usersBlock}>
+            <div>
+                <div className={s.avatar}>
+                    <img alt="avaUsers"></img>
+                
+                </div>
+            
+                <div className={s.buttonFollow}>
+                    <button onClick={followTry}> follow</button>
+
+                </div>
+            </div>
+            
+            <div className={s.usersInfo}>
+
+                 <div>{props.name}</div>
+
+                <div>{props.country},{props.city}</div>
+            
+
+            </div>
+        </div>
+    )
+}
+export default UsersBar;
