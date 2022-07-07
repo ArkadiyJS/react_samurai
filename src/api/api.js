a12f30215bb48323cac77abcbe31254074b405d3
Arkadiy Vologodskiy <Vologodskiy20@gmail.com>
2022-07-07 16:16:27 +0500

    api

--- src/api/api.js
+++ src/api/api.js
@@ -1,19 +1,34 @@
 import * as axios from "axios";
 
 
-const instance =axios.create({
+const instance = axios.create({
   withCredentials: true,
   baseURL: 'https://social-network.samuraijs.com/api/1.0/',
   headers: {
-    "API-KEY":'3a17e2ad-7a32-44d3-a695-70588fd52fad'
+    "API-KEY": '3a17e2ad-7a32-44d3-a695-70588fd52fad'
 
   }
 });
 
+export const usersAPI = {
+  getUsers(currentPage, pageSize) {
+    return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+      .then(response => response.data)
+  },
+  getUsersProfile() {
+    return instance.get('profile/').then(response => response.data)
+  },
 
-export const getUsers=(currentPage,pageSize)=>{
-  return instance.get(`users?page=${currentPage}&count=${pageSize}`).then(response => response.data)
+  followUp(userid) {
+    return instance.post(`follow/${userid}`)
+
+
+  },
+  unFollow(userid) {
+    return instance.delete(`follow/${userid}`)
+  },
+  authLogin(){
+    return instance.get('auth/me');
+  }
+  
 }
-export const getUsersProfile=()=>{
-  return instance.get('profile/').then(response => response.data)
-} 
