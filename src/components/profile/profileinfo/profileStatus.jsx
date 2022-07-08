1ab58a344a1681a63c8494baac8a2fb1e894b86c
Arkadiy Vologodskiy <Vologodskiy20@gmail.com>
2022-07-08 06:22:29 +0500

    local state status

--- src/components/profile/profileinfo/profileStatus.jsx
+++ src/components/profile/profileinfo/profileStatus.jsx
@@ -3,17 +3,47 @@ import React from "react";
 
 
 
-function ProfileStatus (props){
-  return(<>
+class ProfileStatus extends React.Component{
+
+  state={
+    editMode:false
+    
+  }
+
+
+
+
+  activateEditMode=()=>{
+    this.setState({
+      editMode:true,
+    })
+  }
+  deactivateEditMode=()=>{
+    this.setState({
+      editMode:false,
+    })
+  }
+
+
+
+
+  render(){
+
+    return(<>
+
+  {!this.state.editMode &&
     <div>
-      <span>{props.status}</span>
+      <span onDoubleClick={this.activateEditMode}>{this.props.status}</span>
 
     </div>
+    }
+    {this.state.editMode &&
     <div>
-      <input value={props.status}></input>
+      <input autoFocus={true} onBlur={this.deactivateEditMode.bind(this)} value={this.props.status}></input>
 
     </div>
+    }
     </>
   )
-}
+}}
 export default ProfileStatus;
